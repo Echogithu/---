2b519cfda7c4d1add72ca09032ce27ac6a9bada2
caizeping <caizeping@dm-ai.cn>
2021-08-21 18:20:15 +0800

    整理代码

--- 第一章/demo1/index-refactoring.js
+++ 第一章/demo1/index-refactoring.js
@@ -33,6 +33,60 @@ var plays = {
   }
 };
 
+// 打印订单逻辑相关
+function statement(invoice, plays) {
+  let result = `Statement for ${invoice.customer}\n`;
+
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seats\n)`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+
+  return result;
+}
+
+function totalAmount(invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
+  }
+  return result;
+}
+
+// 拆分循环
+function totalVolumeCredits(invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
+// 提取格式化函数
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+}
+
+// 提炼计算观众量积分的逻辑
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  // add volume credits
+  result += Math.max(aPerformance.audience - 30, 0);
+  // add extra credits for every ten comedy attendees
+  // 每十名喜剧观众加一个额外学分
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
+}
+
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
@@ -58,60 +112,6 @@ function amountFor(aPerformance) {
     default:
       throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
   }
-  return result;
-}
-
-// 提炼计算观众量积分的逻辑
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  // add volume credits
-  result += Math.max(aPerformance.audience - 30, 0);
-  // add extra credits for every ten comedy attendees
-  // 每十名喜剧观众加一个额外学分
-  if ("comedy" === playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-// 提取格式化函数
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-  }).format(aNumber / 100);
-}
-
-// 拆分循环
-function totalVolumeCredits(invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function totalAmount(invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-
-function statement(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-
-  for (let perf of invoice.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats\n)`;
-  }
-
-  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
-  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
-
   return result;
 }
 
